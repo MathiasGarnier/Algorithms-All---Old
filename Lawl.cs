e77a187a84bfacc691498d2e1a00b62b53b35627
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-03-21 19:19:47 +0100

    Create Lawl.cs

---
+++ Lawl.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace TestCS {
+
+	class MainClass {
+
+		public static void Main (string[] args) {
+                    Neuro rep = (circonstance) new Neuro(Brain().rep(rep.LOGIQUE, rep.FONDE));
+
+		}
+
+	}
+}
