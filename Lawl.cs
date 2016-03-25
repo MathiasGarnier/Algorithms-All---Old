b419d5a96ecbec6090275d48d86946a8b6a874f1
Mathias Garnier - Yapix <mathias.garnier69000@gmail.com>
2016-03-25 21:46:48 +0100

    Update Lawl.cs

--- Lawl.cs
+++ Lawl.cs
@@ -7,7 +7,7 @@ namespace TestCS {
 	class MainClass {
 
 		public static void Main (string[] args) {
-                    Neuro rep = (circonstance) new Neuro(Brain().rep(rep.LOGIQUE, rep.FONDE));
+                    Neuro rep = ((circonstance) arguments) new Neuro(Brain().rep(rep.LOGIQUE, rep.FONDE));
 
 		}
 
