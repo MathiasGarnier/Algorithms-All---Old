7b96d2cad5007860df6fd8a8724100d77bc11e66
Mathias Garnier <mathias.garnier69000@gmail.com>
2023-01-04 13:37:09 +0100

    Add files via upload

---
+++ VectorAPI.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "VectorAPI.h"
+
+int main() {
+
+    Vector                    v;
+
+    v.length            =     8;
+    v.direction[0]      =   'A';
+    v.direction[1]      =   'B';
+    v.way[0]            =   'A';
+    v.way[1]            =   'B';
+
+    Vector                   v1;
+
+    v1.length           =     8;
+    v1.direction[0]     =   'A';
+    v1.direction[1]     =   'B';
+    v1.way[0]           =   'A';
+    v1.way[1]           =   'B';
+
+    vectorCompare(v, v1);
+
+    //YOLO PATE A L'EAU
+    return 0;
+}
