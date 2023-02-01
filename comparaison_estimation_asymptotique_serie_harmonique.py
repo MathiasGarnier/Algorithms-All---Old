1b2c7e96324da57f05a8cdf39ad007881a993fce
Mathias Garnier <mathias.garnier69000@gmail.com>
2023-02-01 12:33:33 +0100

    Ajout d'estimations

--- comparaison_estimation_asymptotique_serie_harmonique.py
+++ comparaison_estimation_asymptotique_serie_harmonique.py
@@ -13,6 +13,7 @@ def harmonique(n):
     
     return res
 
+
 def estim_1(n):
 
     # serie harmonique = log(n) + gamma + 1/(2n) + o(1/n)
@@ -20,6 +21,14 @@ def estim_1(n):
     gamma = 0.5772156649015328
 
     return np.log(n) + gamma + 1/(2 * n)
+
+def estim_1_1(n):
+    
+    # serie harmonique = log(n+1) + gamma + 1/(2n) - 1/(12n^2) + o(1/n^2)
+
+    gamma = 0.5772156649015328
+    
+    return np.log(n + 1) + gamma + 1/(2 * n) - 1/(12 * n**2)
 
 def estim_2(n):
 
@@ -29,14 +38,26 @@ def estim_2(n):
     
     return np.log(n + 1) + gamma - 1/(2 * n)
 
+def estim_2_2(n):
+
+    # serie harmonique = log(n+1) + gamma - 1/(2n) + 1 /(3n^2) + o(1/n^3)
+
+    gamma = 0.5772156649015328
+    
+    return np.log(n + 1) + gamma - 1/(2 * n) + 1 /(3 * n**2)
+
 def plot_curves():
     
     BORNE = 1000
     t = np.arange(1, BORNE, 1)
 
     plt.plot(t, harmonique(BORNE - 1), label="série harmonique")
+
     plt.plot(t, estim_1(t), label="estim 1")
+    plt.plot(t, estim_1_1(t), label="estim 1_1")
+    
     plt.plot(t, estim_2(t), label="estim 2")
+    plt.plot(t, estim_2_2(t), label="estim 2_2")
 
     leg = plt.legend(loc='lower right')
 
@@ -48,16 +69,24 @@ def plot_diff_estim():
     t = np.arange(1, BORNE, 1)
 
     harm = harmonique(BORNE - 1)
+
     diff1 = abs(harm - estim_1(t))
+    diff1_1 = abs(harm - estim_1_1(t))
+
     diff2 = abs(harm - estim_2(t))
+    diff2_2 = abs(harm - estim_2_2(t))
 
     plt.plot(t, diff1, label="diff estim 1")
+    plt.plot(t, diff1_1, label="diff estim 1_1")
+
     plt.plot(t, diff2, label="diff estim 2")
+    plt.plot(t, diff2_2, label="diff estim 2_2")
 
     leg = plt.legend(loc='lower right')
 
     plt.show()
 
+plot_curves()
 plot_diff_estim()
 
-# Conclusion, mon estimation est un pequeño mieux
+# Conclusion, mon estimation est un pequeño mieux (à l'ordre 1/n au moins ?)
