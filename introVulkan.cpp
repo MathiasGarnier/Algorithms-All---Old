c31908fd1b2ee63767f4c04cee92b69e54f6217c
Mathias Garnier <mathias.garnier69000@gmail.com>
2023-02-04 11:30:42 +0100

    Primi Passi

--- introVulkan.cpp
+++ introVulkan.cpp
@@ -8,9 +8,15 @@
 #include <cstdlib>
 #include <optional>
 
+#include <cstdint> // Necessary for uint32_t
+#include <limits> // Necessary for std::numeric_limits
+#include <algorithm> // Necessary for std::clamp
+
 #include <vector>
 #include <map>
 #include <set>
+
+// Et putain je n'ai même pas fait les verification layers.
 
 class HelloTriangleApplication {
 
@@ -34,6 +40,16 @@ class HelloTriangleApplication {
     // the screen. The swap chain is essentially a queue of images that are waiting to be presented 
     // to the screen. (...) The general purpose of the swap chain is to synchronize the presentation 
     // of images with the refresh rate of the screen."
+    //
+    // "How we can set up the images as render targets and then we start looking into the actual graphics 
+    // pipeline and drawing commands ?"
+    // 
+    // "An image view is sufficient to start using an image as a texture, but it's not quite ready to be 
+    // used as a render target just yet. That requires one more step of indirection, known as a framebuffer. 
+    // But first we'll have to set up the graphics pipeline."
+
+
+
 
 
 
@@ -107,6 +123,15 @@ class HelloTriangleApplication {
             std::vector<VkPresentModeKHR> presentModes;
         };
 
+        VkSwapchainKHR swapChain;
+        std::vector<VkImage> swapChainImages;
+        VkFormat swapChainImageFormat;
+        VkExtent2D swapChainExtent;
+        std::vector<VkImageView> swapChainImageViews; // 2D texture depth texture without any mipmapping levels
+
+
+
+
         void initWindow() {
 
             glfwInit();
@@ -123,6 +148,8 @@ class HelloTriangleApplication {
             createSurface();
             pickPhysicalDevice();
             createLogicalDevice();
+            createSwapChain();
+            createImageViews();
         }
 
         void mainLoop() {
@@ -135,7 +162,11 @@ class HelloTriangleApplication {
 
         void cleanup() {
 
-            // D'abord détruire le device et la surface... et ensuite l'instance !
+            // D'abord détruire la swap chain, le device et la surface... et ensuite l'instance !
+            for (auto imageView : swapChainImageViews) {
+                vkDestroyImageView(logicalDevice, imageView, nullptr);
+            }
+            vkDestroySwapchainKHR(logicalDevice, swapChain, nullptr);
             vkDestroyDevice(logicalDevice, nullptr);
             vkDestroySurfaceKHR(instance, surface, nullptr);
             vkDestroyInstance(instance, nullptr);
@@ -350,7 +381,7 @@ class HelloTriangleApplication {
                 }
 
                 VkBool32 presentSupport = false;
-                // --> est-ce supporté ?
+                // --> est-ce supporté ? --> 
                 vkGetPhysicalDeviceSurfaceSupportKHR(device, i, surface, &presentSupport);
 
                 if (presentSupport) {
@@ -449,6 +480,175 @@ class HelloTriangleApplication {
             return details;
         }
 
+        VkSurfaceFormatKHR chooseSwapSurfaceFormat(const std::vector<VkSurfaceFormatKHR>& availableFormats) {
+
+            for (const auto& availableFormat : availableFormats) {
+                if (availableFormat.format == VK_FORMAT_B8G8R8A8_SRGB && availableFormat.colorSpace == VK_COLOR_SPACE_SRGB_NONLINEAR_KHR) {
+                    return availableFormat;
+                }
+            }
+
+            return availableFormats[0]; 
+            // On pourrait aussi écrire une fonction qui note et classe les formats en fonction 
+            // de notre demande (puis retourner le format qui y correspondrait le mieux)
+        }
+
+        VkPresentModeKHR chooseSwapPresentMode(const std::vector<VkPresentModeKHR>& availablePresentModes) {
+
+            //      (*) VK_PRESENT_MODE_IMMEDIATE_KHR: Images submitted by your application are transferred to the screen 
+            // right away, which may result in tearing.
+            //      (*) VK_PRESENT_MODE_FIFO_KHR: The swap chain is a queue where the display takes an image from the front 
+            // of the queue when the display is refreshedand the program inserts rendered images at the back of the queue. 
+            // If the queue is full then the program has to wait.This is most similar to vertical sync as found in modern 
+            // games.The moment that the display is refreshed is known as "vertical blank".
+            //      (*) VK_PRESENT_MODE_FIFO_RELAXED_KHR : This mode only differs from the previous one if the application 
+            // is lateand the queue was empty at the last vertical blank.Instead of waiting for the next vertical blank, 
+            // the image is transferred right away when it finally arrives.This may result in visible tearing.
+            //      (*) VK_PRESENT_MODE_MAILBOX_KHR : This is another variation of the second mode. Instead of blocking 
+            // the application when the queue is full, the images that are already queued are simply replaced with the 
+            // newer ones. This mode can be used to render frames as fast as possible while still avoiding tearing, 
+            // resulting in fewer latency issues than standard vertical sync. This is commonly known as "triple buffering", 
+            // although the existence of three buffers alone does not necessarily mean that the framerate is unlocked.
+            
+            for (const auto& availablePresentMode : availablePresentModes) {
+                if (availablePresentMode == VK_PRESENT_MODE_MAILBOX_KHR) {
+                    return availablePresentMode;
+                }
+            }
+
+            return VK_PRESENT_MODE_FIFO_KHR;
+        }
+
+        VkExtent2D chooseSwapExtent(const VkSurfaceCapabilitiesKHR& capabilities) {
+
+            // Vulkan works with pixels (not with screen coordinates)
+
+            if (capabilities.currentExtent.width != std::numeric_limits<uint32_t>::max()) {
+                return capabilities.currentExtent;
+            }
+            else {
+                int width, height;
+                glfwGetFramebufferSize(window, &width, &height);
+
+                VkExtent2D actualExtent = {
+                    static_cast<uint32_t>(width),
+                    static_cast<uint32_t>(height)
+                };
+
+                actualExtent.width = std::clamp(actualExtent.width, capabilities.minImageExtent.width, capabilities.maxImageExtent.width);
+                actualExtent.height = std::clamp(actualExtent.height, capabilities.minImageExtent.height, capabilities.maxImageExtent.height);
+
+                return actualExtent;
+            }
+        }
+
+        void createSwapChain() {
+
+            SwapChainSupportDetails swapChainSupport = querySwapChainSupport(physicalDevice);
+            VkSurfaceFormatKHR surfaceFormat = chooseSwapSurfaceFormat(swapChainSupport.formats);
+            VkPresentModeKHR presentMode = chooseSwapPresentMode(swapChainSupport.presentModes);
+            VkExtent2D extent = chooseSwapExtent(swapChainSupport.capabilities);
+
+            uint32_t imageCount = swapChainSupport.capabilities.minImageCount + 1;
+
+            if (swapChainSupport.capabilities.maxImageCount > 0 && imageCount > swapChainSupport.capabilities.maxImageCount) {
+                imageCount = swapChainSupport.capabilities.maxImageCount;
+            }
+
+            VkSwapchainCreateInfoKHR createinfo{};
+
+            createinfo.sType = VK_STRUCTURE_TYPE_SWAPCHAIN_CREATE_INFO_KHR;
+            createinfo.surface = surface;
+            createinfo.minImageCount = imageCount;
+            createinfo.imageFormat = surfaceFormat.format;
+            createinfo.imageColorSpace = surfaceFormat.colorSpace;
+            createinfo.imageExtent = extent;
+            createinfo.imageArrayLayers = 1; // = 1 sauf si 3D stéréoscopique
+            createinfo.imageUsage = VK_IMAGE_USAGE_COLOR_ATTACHMENT_BIT;
+
+            QueueFamilyIndices indices = findQueueFamilies(physicalDevice);
+            uint32_t queueFamilyIndices[] = { indices.graphicsfamily.value(), indices.presentfamily.value() };
+
+            if (indices.graphicsfamily != indices.presentfamily) {
+                createinfo.imageSharingMode = VK_SHARING_MODE_CONCURRENT;
+                createinfo.queueFamilyIndexCount = 2;
+                createinfo.pQueueFamilyIndices = queueFamilyIndices;
+            }
+            else {
+                createinfo.imageSharingMode = VK_SHARING_MODE_EXCLUSIVE;
+                createinfo.queueFamilyIndexCount = 0; // Optional
+                createinfo.pQueueFamilyIndices = nullptr; // Optional
+            }
+
+            //      (*) VK_SHARING_MODE_EXCLUSIVE: An image is owned by one queue family at a time and ownership 
+            // must be explicitly transferred before using it in another queue family. This option offers the best 
+            // performance.
+            //      (*) VK_SHARING_MODE_CONCURRENT: Images can be used across multiple queue families without explicit 
+            // ownership transfers.
+
+            createinfo.preTransform = swapChainSupport.capabilities.currentTransform;
+            createinfo.compositeAlpha = VK_COMPOSITE_ALPHA_OPAQUE_BIT_KHR;   // = 0x00000001 
+
+            createinfo.presentMode = presentMode;
+            createinfo.clipped = VK_TRUE;   // wooow, une histoire de calcul des pixels que l'on ne voit pas
+                                            // si par exemple une fenêtre obstrue la vision.
+
+
+            // With Vulkan it's possible that your swap chain becomes invalid or unoptimized while your application 
+            // is running, for example because the window was resized. In that case the swap chain actually needs to
+            // be recreated from scratch and a reference to the old one must be specified in this field.
+            createinfo.oldSwapchain = VK_NULL_HANDLE;
+
+            if (vkCreateSwapchainKHR(logicalDevice, &createinfo, nullptr, &swapChain) != VK_SUCCESS) {
+
+                throw std::runtime_error("Impossible de créer une swap chain.");
+            }
+
+            vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, nullptr);
+            swapChainImages.resize(imageCount);
+            vkGetSwapchainImagesKHR(logicalDevice, swapChain, &imageCount, swapChainImages.data());
+
+            swapChainImageFormat = surfaceFormat.format;
+            swapChainExtent = extent;
+        }
+
+        void createImageViews() {
+
+            swapChainImageViews.resize(swapChainImages.size());
+
+            for (size_t i = 0; i < swapChainImages.size(); i++) {
+
+                VkImageViewCreateInfo createinfo{};
+
+                createinfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+                createinfo.image = swapChainImages[i];
+                createinfo.viewType = VK_IMAGE_VIEW_TYPE_2D; // viewType : treat images as 1D textures, 
+                                                             // 2D textures, 3D textures and cube maps.
+                createinfo.format = swapChainImageFormat;
+
+                createinfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY; // Bidouiller ça !
+                createinfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY; // Bidouiller ça !
+                createinfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY; // Bidouiller ça !
+                createinfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY; // Bidouiller ça !
+
+
+                createinfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+                createinfo.subresourceRange.baseMipLevel = 0;
+                createinfo.subresourceRange.levelCount = 1;
+                createinfo.subresourceRange.baseArrayLayer = 0;
+                createinfo.subresourceRange.layerCount = 1;
+
+                // "If you were working on a stereographic 3D application, then you would create 
+                // a swap chain with multiple layers. You could then create multiple image views 
+                // for each image representing the views for the left and right eyes by accessing 
+                // different layers."
+
+                if (vkCreateImageView(logicalDevice, &createinfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+                    
+                    throw std::runtime_error("Impossible de créer une ImageView.");
+                }
+            }
+        }
 };
 
 
