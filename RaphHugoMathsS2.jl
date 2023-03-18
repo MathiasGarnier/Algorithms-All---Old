a5d9322acd05a486ab12cd868ae347d26caae995
Mathias Garnier <mathias.garnier69000@gmail.com>
2023-03-18 12:16:42 +0100

    Update RaphHugoMathsS2.jl

--- RaphHugoMathsS2.jl
+++ RaphHugoMathsS2.jl
@@ -7,7 +7,8 @@
 # * Ensuite lier interA et interB aux bornes : xs et xlims()
 # * Une fois le problème de la fonction réglée -> Accepter une fonction écrite en latex
 
-
+primVal = 0
+lastVal = 1
 
 using GLMakie
 using PlotlyJS # To get the function minimum and maximum
@@ -19,23 +20,28 @@ interA_tb = Textbox(fig[2, 1], placeholder = "      [a, b]      ") #, validator 
 #interB_tb = Textbox(fig[2, 2], placeholder = "b", validator = Float64, tellwidth = false)
 interB_funcExpression = Textbox(fig[2, 9:10], placeholder = "      Expression      ") #, validator = String, tellwidth = false)
 
-sl_n = Slider(fig[2, 3:8], range = 0:1:10, startvalue = 1)
+sl_n = Slider(fig[2, 3:8], range = 0:1:20, startvalue = 1)
 
 # * Observables
-interA = Observable(0)
+interA = Observable("")
 #interB = Observable(1.0)
 interfuncExpr = Observable("")
 slider_val = Observable(sl_n.startvalue)
 
 # * Interactions
 on(interA_tb.stored_string) do s
-    interA[] = parse(String, s)
+    primVal = 3
+    lastVal = 4
+    
 end
-#on(interB_tb.stored_string) do s
-#    interB[] = parse(Float64, s)
-#end
+on(interB_funcExpression.stored_string) do s
+
+    return nothing    
+end
 on(sl_n.value) do s
     slider_val[] = s
+    println(slider_val[])
+    println(getindex(slider_val[]))
 end
 
 
@@ -45,18 +51,59 @@ menuOption = Menu(fig[3, 9:10], options = ["Options", "Symbolique", "Speed-up nu
 menuModeApprox.is_open = false
 menuError.is_open = false
 
-xs = 0:0.001:1
+xs = primVal:0.001:lastVal
 
 # lift c'est pour dire que c'est reactif
+
 function fPlot()
-    lifted = @lift($xs .^ 3 * $slider_val[])
+    lifted = @lift($xs .^ 4 * $slider_val[])
     #lifted = @lift(gen_bernstein($xs, $slider_val[], g_1))
     return lifted
 end
 
 
-l=lines(fig[1, 1:10], xs, fPlot())
-xlims!(0, 1)
+function g_1(x)
+    return x * sin(x)
+end
+
+function g_2(x)
+    return x^2
+end
+
+function bernstein_poly(x, order)
+
+    res = []
+    for k in 0:order
+        append!(res, binomial(order, k) .* x^(order .- k) .* (1 .- x) .^k)
+    end
+
+    return res
+end
+
+function bernstein_func(x, order)
+
+    res = []
+    for k in 0:order
+        res += g_1.(k / order) .* bernstein_poly.(x, order)
+    end
+    println(res)
+    return res
+end
+
+
+ax = Axis(fig[1, 1:10])
+lines!(ax, xs, g_1.(xs), label="x")
+#lines!(ax, xs, g_2.(xs), label="x^2")
+lines!(ax, xs, @lift(sum([g_1(k / getindex($slider_val)) .* binomial(getindex($slider_val), k) .* xs .^ (getindex($slider_val) - k) .* (1 .- xs) .^ k; for k in 0:getindex($slider_val)])), label="APPROX")
+
+#x= sum([@lift(binomial($slider_val[], k) .* xs .^($slider_val[] - k) .* (1 - xs) .^ k); for k in 0:$slider_val[]])
+
+
+#l2=lines(fig[1, 1:10], xs, fPlot())
+
+xlims!(primVal, lastVal)
 ylims!(0, 1)
+
+axislegend()
 
 fig
